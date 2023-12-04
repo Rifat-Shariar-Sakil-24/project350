916dee4b0e423e7815e159db9c554b60e2edc813
Rifat Shariar Sakil <rifatsakilmi1309@gmail.com>
2023-12-04 11:22:40 +0600

    sql changed

--- bookdistribution.sql
+++ bookdistribution.sql
@@ -28,39 +28,39 @@ CREATE TABLE books (
 );
 
 
-CREATE TABLE `students` (
-  `class` int(11) NOT NULL,
-  `first_name` varchar(20) NOT NULL,
-  `last_name` varchar(20) NOT NULL,
-  `roll` int(11) NOT NULL,
-  `phone` int(11) NOT NULL,
-  `father_name` varchar(20) NOT NULL,
-  `mother_name` varchar(25) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `comment` varchar(255) NOT NULL,
-  PRIMARY KEY (`roll`, `class`)
-);
+-- CREATE TABLE `students` (
+--   `class` int(11) NOT NULL,
+--   `first_name` varchar(20) NOT NULL,
+--   `last_name` varchar(20) NOT NULL,
+--   `roll` int(11) NOT NULL,
+--   `phone` int(11) NOT NULL,
+--   `father_name` varchar(20) NOT NULL,
+--   `mother_name` varchar(25) NOT NULL,
+--   `address` varchar(255) NOT NULL,
+--   `comment` varchar(255) NOT NULL,
+--   PRIMARY KEY (`roll`, `class`)
+-- );
 
 
-CREATE TABLE `distributed_books` (
+-- CREATE TABLE `distributed_books` (
 
-  `class` int(11) NOT NULL,
-  `roll` int(11) NOT NULL,
-  `bangla` tinyint(1) NOT NULL,
-  `english` tinyint(1) NOT NULL,
-  `math` tinyint(1) NOT NULL,
-  `science` tinyint(1) NOT NULL,
-  `social_science` tinyint(1) NOT NULL,
-  `religion` tinyint(1) DEFAULT NULL,
-  `comment` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`roll`, `class`),
+--   `class` int(11) NOT NULL,
+--   `roll` int(11) NOT NULL,
+--   `bangla` tinyint(1) NOT NULL,
+--   `english` tinyint(1) NOT NULL,
+--   `math` tinyint(1) NOT NULL,
+--   `science` tinyint(1) NOT NULL,
+--   `social_science` tinyint(1) NOT NULL,
+--   `religion` tinyint(1) DEFAULT NULL,
+--   `comment` varchar(255) DEFAULT NULL,
+--   PRIMARY KEY (`roll`, `class`),
 
-  CONSTRAINT `fk_distributed_books_students`
-    FOREIGN KEY (`roll`, `class`)
-    REFERENCES `students` (`roll`, `class`)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
+--   CONSTRAINT `fk_distributed_books_students`
+--     FOREIGN KEY (`roll`, `class`)
+--     REFERENCES `students` (`roll`, `class`)
+--     ON UPDATE CASCADE
+--     ON DELETE CASCADE
+-- );
 
 
 
