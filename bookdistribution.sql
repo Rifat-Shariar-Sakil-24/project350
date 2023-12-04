32888de17e3cc41e30db6db5dd921dc255a7d3db
Rifat Shariar Sakil <rifatsakilmi1309@gmail.com>
2023-12-04 12:28:27 +0600

    sql code

--- bookdistribution.sql
+++ bookdistribution.sql
@@ -1,78 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 04, 2023 at 06:44 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE users (
-  id int(11) NOT NULL,
-  email varchar(20) NOT NULL,
-  password varchar(20) NOT NULL
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookdistribution`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `book_date` date NOT NULL,
+  `year` int(4) NOT NULL,
+  `class` varchar(50) NOT NULL,
+  `subject_name` varchar(100) NOT NULL,
+  `number_of_books` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
 
-INSERT INTO users (id, email, password) VALUES
-(1, 'admin@gmail.com', 'password');
+--
+-- Table structure for table `books2`
+--
 
--- CREATE TABLE books (
---     book_date DATE NOT NULL,
---     class VARCHAR(50) NOT NULL,
---     `year` int(4) NOT NULL,
---     subject_name VARCHAR(100) NOT NULL,
---     number_of_books INT NOT NULL
--- );
-CREATE TABLE books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    book_date DATE NOT NULL,
-    `year` INT(4) NOT NULL,
-    class VARCHAR(50) NOT NULL,
-    
-    subject_name VARCHAR(100) NOT NULL,
-    number_of_books INT NOT NULL
-);
+CREATE TABLE `books2` (
+  `book_date` date NOT NULL,
+  `class` varchar(50) NOT NULL,
+  `subject_name` varchar(100) NOT NULL,
+  `number_of_books` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `books2`
+--
 
--- CREATE TABLE `students` (
---   `class` int(11) NOT NULL,
---   `first_name` varchar(20) NOT NULL,
---   `last_name` varchar(20) NOT NULL,
---   `roll` int(11) NOT NULL,
---   `phone` int(11) NOT NULL,
---   `father_name` varchar(20) NOT NULL,
---   `mother_name` varchar(25) NOT NULL,
---   `address` varchar(255) NOT NULL,
---   `comment` varchar(255) NOT NULL,
---   PRIMARY KEY (`roll`, `class`)
--- );
+INSERT INTO `books2` (`book_date`, `class`, `subject_name`, `number_of_books`) VALUES
+('2023-11-09', '1', 'bangla', 12);
 
+-- --------------------------------------------------------
 
--- CREATE TABLE `distributed_books` (
+--
+-- Table structure for table `distributed_books`
+--
 
---   `class` int(11) NOT NULL,
---   `roll` int(11) NOT NULL,
---   `bangla` tinyint(1) NOT NULL,
---   `english` tinyint(1) NOT NULL,
---   `math` tinyint(1) NOT NULL,
---   `science` tinyint(1) NOT NULL,
---   `social_science` tinyint(1) NOT NULL,
---   `religion` tinyint(1) DEFAULT NULL,
---   `comment` varchar(255) DEFAULT NULL,
---   PRIMARY KEY (`roll`, `class`),
+CREATE TABLE `distributed_books` (
+  `class` int(11) NOT NULL,
+  `roll` int(11) NOT NULL,
+  `year` int(4) NOT NULL,
+  `subjects` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`subjects`)),
+  `comment` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---   CONSTRAINT `fk_distributed_books_students`
---     FOREIGN KEY (`roll`, `class`)
---     REFERENCES `students` (`roll`, `class`)
---     ON UPDATE CASCADE
---     ON DELETE CASCADE
--- );
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `students`
+--
 
-
-
-
-
-
-
-
-
-
--- Create the students table with the new year column
 CREATE TABLE `students` (
   `class` int(11) NOT NULL,
   `first_name` varchar(20) NOT NULL,
@@ -83,54 +86,83 @@ CREATE TABLE `students` (
   `father_name` varchar(20) NOT NULL,
   `mother_name` varchar(25) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `comment` varchar(255) NOT NULL,
-  PRIMARY KEY (`roll`, `class`, `year`)
-);
+  `comment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- -- Create the distributed_books table with the new year column and foreign key
--- CREATE TABLE `distributed_books` (
---   `class` int(11) NOT NULL,
---   `roll` int(11) NOT NULL,
---   `year` int(4) NOT NULL,
---   `bangla` tinyint(1) NOT NULL,
---   `english` tinyint(1) NOT NULL,
---   `math` tinyint(1) NOT NULL,
---   `science` tinyint(1) NOT NULL,
---   `social_science` tinyint(1) NOT NULL,
---   `religion` tinyint(1) DEFAULT NULL,
---   `comment` varchar(255) DEFAULT NULL,
---   PRIMARY KEY (`roll`, `class`, `year`),
---   CONSTRAINT `fk_distributed_books_students`
---     FOREIGN KEY (`roll`, `class`, `year`)
---     REFERENCES `students` (`roll`, `class`, `year`)
---     ON UPDATE CASCADE
---     ON DELETE CASCADE
--- );
+-- --------------------------------------------------------
 
--- CREATE TABLE `distributed_books` (
---   `class` INT(11) NOT NULL,
---   `roll` INT(11) NOT NULL,
---   `year` INT(4) NOT NULL,
---   `subjects` JSON NOT NULL,
---   `comment` VARCHAR(255) DEFAULT NULL,
---   PRIMARY KEY (`roll`, `class`, `year`),
---   CONSTRAINT `fk_distributed_books_students`
---     FOREIGN KEY (`roll`, `class`, `year`)
---     REFERENCES `students` (`roll`, `class`, `year`)
---     ON UPDATE CASCADE
---     ON DELETE CASCADE
--- );
-CREATE TABLE `distributed_books` (
-  `class` INT(11) NOT NULL,
-  `roll` INT(11) NOT NULL,
-  `year` INT(4) NOT NULL,
-  `subjects` JSON NOT NULL,
-  `comment` VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (`roll`, `class`, `year`),
-  CONSTRAINT `fk_distributed_books_students`
-    FOREIGN KEY (`roll`, `class`, `year`)
-    REFERENCES `students` (`roll`, `class`, `year`)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
+--
+-- Table structure for table `users`
+--
 
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+(1, 'admin@gmail.com', 'password');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `distributed_books`
+--
+ALTER TABLE `distributed_books`
+  ADD PRIMARY KEY (`roll`,`class`,`year`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`roll`,`class`,`year`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `distributed_books`
+--
+ALTER TABLE `distributed_books`
+  ADD CONSTRAINT `fk_distributed_books_students` FOREIGN KEY (`roll`,`class`,`year`) REFERENCES `students` (`roll`, `class`, `year`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
